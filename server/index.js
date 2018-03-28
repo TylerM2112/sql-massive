6258e0bc00ee67683cc56fd99b81893bbd86cd90
Tyler <millert2420@gmail.com>
2018-03-28 11:41:46 -0700

    pushing

--- server/index.js
+++ server/index.js
@@ -19,9 +19,18 @@ app.get('/api/heroes', (req, res) => {
         console.log('Oh no! An error has happened!', error);
         res.status(500).json({ message: 'Bummer!' })
     });
- })
+})
+ 
+app.get('/api/awesome_heroes', (req, res) => {
+    req.app.get('db').get_awesome_heroes({ powers: req.query.powers, real_age: req.query.real_age}).then(heroes => {
+        res.status(200).json(heroes);
+    }).catch(error => {
+        console.log('Oh no! An error has happened!', error);
+        res.status(500).json({ message: 'Bummer!' })
+    });
+})
 
 const port = 4000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-});
+})
